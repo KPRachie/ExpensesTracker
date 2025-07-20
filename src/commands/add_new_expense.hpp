e1e77774d635cffa972a62dee9a2c5a01564cede
kathi <kathi@KPRACHIE>
2025-07-20 13:17:42 +0300

    Added parse function for /add_new_expense command and finished add_new_expense function. (close #22, close #11)

--- src/commands/add_new_expense.hpp
+++ src/commands/add_new_expense.hpp
@@ -1,8 +1,16 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 #include "command.hpp"
+
+struct expense_t {
+	int64_t tg_id;
+	int64_t cat_id;
+	std::string cost;
+	std::string comment;
+};
 
 class AddNewExpense : public ICommand {
 public:
@@ -11,16 +19,10 @@ public:
 	void exec(TgBot::Message::Ptr& message) override;
 
 private:
-	void add_expense(int64_t tg_id, int64_t cat_id, int64_t cost, std::string comment);
+	void add_expense(std::vector<expense_t> expenses);
+	std::vector<expense_t> parse(int64_t tg_id, std::string message);
+	int64_t get_category_id(std::string category);
 
 	bool m_received_first_message;
-
 };
 
-struct expense_t {
-	int64_t id;
-	int64_t tg_id;
-	int64_t cat_id;
-	int64_t cost;
-	std::string comment;
-};
