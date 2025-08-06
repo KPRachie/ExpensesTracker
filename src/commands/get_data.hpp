39892b3cfa480cb916bc850a1a2e877e5eaf5a8d
kathi <kathi@KPRACHIE>
2025-08-06 16:08:10 +0300

    Finished /add_data command. (close #10)

--- src/commands/get_data.hpp
+++ src/commands/get_data.hpp
@@ -17,6 +17,7 @@ private:
 	std::vector<std::string> parse(std::string message);
 	std::unordered_map<std::string, int64_t> get_cat_ids(const std::vector<std::string>& categories);
 	std::unordered_map<int64_t, std::string> get_cat_names(const std::vector<int64_t>& cat_ids);
+	std::vector<int64_t> get_all_cat_ids(int64_t tg_id);
 	std::unordered_map<int64_t, int64_t> process(int64_t tg_id, const std::vector<std::string>& categories = {});
 	std::string prepare_data(std::unordered_map<int64_t, int64_t> expenses);
 };
